b2c8978c79196d95ab4ab56d3ca4b38368282db9
TheNotorious <georgi_747@mail.ru>
2020-11-24 01:47:11 +0300

    task2

--- src/ru/geekbrains/lesson1/lessonOne.java
+++ src/ru/geekbrains/lesson1/lessonOne.java
@@ -4,7 +4,20 @@ public class lessonOne
 {
     public static void main(String[] args)
     {
-        
+        byte b = 1;
+        short s = 2;
+        int i = 3;
+        long l = 4;
+
+        double d = 1.5;
+        float f = 1.6f;
+
+        char c = 'c';
+
+        boolean bool = false;
+
+
+
     }
 
 
