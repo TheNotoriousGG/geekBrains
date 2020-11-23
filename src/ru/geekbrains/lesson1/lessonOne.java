b7b0114101f0ef5216446353311a08985162bd6b
TheNotorious <georgi_747@mail.ru>
2020-11-24 02:16:43 +0300

    task4

--- src/ru/geekbrains/lesson1/lessonOne.java
+++ src/ru/geekbrains/lesson1/lessonOne.java
@@ -16,9 +16,14 @@ public class lessonOne
 
         boolean bool = false;
 
-        
+        //task3
         double result = calculate(2, 4,1,2);
         System.out.println(result);
+
+        //task4
+        boolean comp = comparison(3,15);
+        System.out.println(comp);
+
 
 
 
@@ -27,6 +32,20 @@ public class lessonOne
     static double calculate(double a,double b,double c,double d)
     {
         return a * (b + (c / d));
+    }
+
+    static boolean comparison(int leftOper,int rightOper)
+    {
+        int sum = leftOper + rightOper;
+
+        if(sum > 10 && sum <= 20)
+            {
+               return  true;
+            }
+        else
+            {
+                return false;
+            }
     }
 
 
