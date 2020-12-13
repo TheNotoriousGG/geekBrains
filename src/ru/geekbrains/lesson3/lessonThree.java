7e5bd740aba2615f5940bee36a6ecf91c522ee6f
TheNotorious <georgi_747@mail.ru>
2020-12-13 22:42:53 +0300

    Исполненное третье ДЗ

---
+++ src/ru/geekbrains/lesson3/lessonThree.java
@@ -0,0 +1,71 @@
+package ru.geekbrains.lesson3;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class lessonThree
+{
+    public static void main(String[] args)
+    {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+                          "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut",
+                          "olive","pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        guessTheWord(words);
+
+    }
+
+    public static void guessTheWord(String[] words)
+    {
+        System.out.println("Компьютер загадает слово из массива. Цель вашей игры - угадать его)");
+        Random rnd = new Random();
+        Scanner scan = new Scanner(System.in);
+        int rndNum = rnd.nextInt(words.length - 1);
+        int shortLength;
+
+        String hiddenWord = words[rndNum];
+        do
+            {
+            String resString = "";
+
+            System.out.println("Введите предполагаемое слово:");
+            String myWord = scan.nextLine().toLowerCase();
+            if (myWord.length() <= hiddenWord.length())
+            {
+                shortLength = myWord.length();
+            }
+            else
+            {
+                shortLength = hiddenWord.length();
+            }
+
+            if (hiddenWord.equals(myWord))
+            {
+                System.out.println("Поздравляю!!!!!!!Вы угадали слово " + myWord);
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < shortLength; i++)
+                {
+                    if (hiddenWord.charAt(i) == myWord.charAt(i))
+                    {
+                        resString += hiddenWord.charAt(i);
+                    }
+                    else
+                    {
+                        resString += '#';
+                    }
+                }
+
+                resString += "#############";
+            }
+            System.out.println("Вы не угадали(... Все совпадения букв указаны ниже. (# - засекреченный символ)");
+            System.out.println(resString);
+        }
+        while(true);
+    }
+
+}
